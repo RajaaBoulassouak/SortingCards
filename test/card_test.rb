41b8ce40a70c478479f3225d5b46bf1bf6137963
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-07-02 17:48:01 -0600

    Initial first commit

---
+++ test/card_test.rb
@@ -0,0 +1,18 @@
+require "./lib/card"
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class CardTest < Minitest::Test
+
+  def test_it_exists
+    card = Card.new("10", "Hearts")
+    assert_instance_of Card, card
+  end
+
+  def test_card_has_attributes
+    result = Card.new("10", "Hearts")
+    assert_equal "10", result.value
+    assert_equal "Hearts", result.suit
+  end
+
+end
---
+++ test/guess_test.rb
@@ -0,0 +1,46 @@
+require "./lib/guess"
+require "./lib/card"
+require 'minitest/autorun'
+require 'minitest/pride'
+require "pry"
+
+class GuessTest < Minitest::Test
+
+  def test_it_exists
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_instance_of Guess, guess
+  end
+
+  def test_card_has_attributes
+    card = Card.new("10", "Hearts")
+    result = Guess.new("10 of Hearts", card)
+    assert_equal "10 of Hearts", result.response
+    assert_equal card, result.card
+  end
+
+  def test_correct_1
+    card = Card.new("10", "Hearts")
+    result = Guess.new("10 of Hearts", card)
+    assert_equal true, result.correct?
+  end
+
+  def test_correct_2
+    card = Card.new("10", "Hearts")
+    result = Guess.new("2 of Clubs", card)
+    assert_equal false, result.correct?
+  end
+
+  def test_feedback_1
+    card = Card.new("10","Hearts")
+    result = Guess.new("10 of Hearts", card)
+    assert_equal "Correct!", result.feedback
+  end
+
+  def test_feedback_2
+    card = Card.new("10","Hearts")
+    result = Guess.new("2 of Clubs", card)
+    assert_equal "Incorrect.", result.feedback
+  end
+
+end
